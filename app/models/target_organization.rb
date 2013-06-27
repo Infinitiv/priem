9115fcd20eb77253d1840051fe80a4aec0ceacd3
Vladimir Markovnin <markovnin@gmail.com>
2013-06-27 10:44:02 +0400

    Добавление seed для организаций целевого приема

--- app/models/target_organization.rb
+++ app/models/target_organization.rb
@@ -1,5 +1,5 @@
 class TargetOrganization < ActiveRecord::Base
   belongs_to :competitive_group
-  attr_accessible :target_organization_name
+  attr_accessible :target_organization_name, :competitive_group_id
   has_many :competitive_group_target_items
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,25 @@
+# encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-Query.create([{name: 'checkapplication'}, {name: 'delete'}, {name: 'delete/result'}, {name: 'dictionary'}, {name: 'dictionarydetails'}, {name: 'import'}, {name: 'import/result'}, {name: 'validate'}, {name: 'institutioninfo'}, {name: 'test/checkapplication'}, {name: 'test/delete'}, {name: 'test/dictionary'}, {name: 'test/dictionarydetails'}, {name: 'test/import'}])
+#   cities = City.create([{ target_organization_name: 'Chicago' }, { target_organization_name: 'Copenhagen' }])
+#   Mayor.create(target_organization_name: 'Emanuel', city: cities.first)
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Брянской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Владимирской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Вологодской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Ивановской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Калужской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Костромской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Липецкой области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Департамент здравоохранения Тульской области")
+TargetOrganization.create(competitive_group_id: 1, target_organization_name: "Федеральное медико-биологическое агентство")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Департамент здравоохранения Брянской области")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Департамент здравоохранения Владимирской области")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Департамент здравоохранения Ивановской области")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Департамент здравоохранения Калужской области")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Департамент здравоохранения Костромской области")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Департамент здравоохранения Тульской области")
+TargetOrganization.create(competitive_group_id: 2, target_organization_name: "Федеральное медико-биологическое агентство")
+TargetOrganization.create(competitive_group_id: 3, target_organization_name: "Департамент здравоохранения Ивановской области")
